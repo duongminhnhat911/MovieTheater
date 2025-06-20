019993246f488bb94d38337a4c9eaa0174c26ec6
duongminhnhat911 <duongminhnhat911@gmail.com>
2025-06-20 09:58:03 +0700

    Xóa toàn bộ code trên nhánh main

--- Models/HomeViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieBookingWeb.Models
-{
-    public class HomeViewModel
-    {
-        public List<Film>? Carousel { get; set; }
-        public List<Film>? NowShowing { get; set; }
-        public List<Film>? ComingSoon { get; set; }
-    }
-}
