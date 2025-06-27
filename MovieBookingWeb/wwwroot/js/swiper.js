1c92e4723df18608b9a74f10fe80f49b3e884a33
duongminhnhat911 <duongminhnhat911@gmail.com>
2025-06-27 10:53:52 +0700

    Thêm cửa sổ xem trailer và pagination

--- MovieBookingWeb/wwwroot/js/swiper.js
+++ MovieBookingWeb/wwwroot/js/swiper.js
@@ -3,17 +3,17 @@ const swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     slidesPerView: 1,
     spaceBetween: 0,
-    autoplay: { 
-        delay: 4000, 
-        disableOnInteraction: false 
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false
     },
-    navigation: { 
-        nextEl: '.arrow-btn.next', 
-        prevEl: '.arrow-btn.prev' 
+    navigation: {
+        nextEl: '.arrow-btn.next',
+        prevEl: '.arrow-btn.prev'
     },
     effect: 'fade',
-    fadeEffect: { 
-        crossFade: true 
+    fadeEffect: {
+        crossFade: true
     },
 });
 
