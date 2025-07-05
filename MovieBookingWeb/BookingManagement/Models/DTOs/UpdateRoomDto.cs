ce062a921ef2615b269abaee8a88156f8b82e9db
Dang Chi Vi <dangchivi63@gmail.com>
2025-07-06 02:35:38 +0700

    code

--- MovieBookingWeb/BookingManagement/Models/DTOs/UpdateRoomDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookingManagement.Models.DTOs
-{
-    public class UpdateRoomDto
-    {
-        public string? RoomName { get; set; }
-        public bool? Status { get; set; }
-    }
-}
--- MovieBookingWeb/BookingManagement/Models/Entities/Enums/SeatStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookingManagement.Models.Entities.Enums
-{
-    public enum SeatStatus
-    {
-        Available = 0,  // Ghế trống, có thể chọn
-        Held = 1,       // Đang được giữ tạm thời (5-10 phút)
-        Booked = 2      // Đã đặt chính thức, không thể chọn
-    }
-}
