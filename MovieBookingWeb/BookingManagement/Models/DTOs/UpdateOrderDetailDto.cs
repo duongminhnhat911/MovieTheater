d3a14fc09112ae55e97bc5d36d2c185bc18d62c2
duongminhnhat911 <duongminhnhat911@gmail.com>
2025-07-09 13:36:21 +0700

    Clear

--- MovieBookingWeb/BookingManagement/Models/DTOs/UpdateOrderDetailDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookingManagement.Models.DTOs
-{
-    public class UpdateOrderDetailDto
-    {
-        public int? SeatId { get; set; }
-        public int? ShowtimeId { get; set; }
-        public int? Price { get; set; }
-    }
-}
